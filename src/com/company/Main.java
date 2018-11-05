e3e08874b4f259c7f6a746a7f78b84492e1602c6
gramsey1 <graceramsey312@yahoo.com>
2018-11-05 11:20:52 -0500

    hate

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+
+            {
+                Scanner sf = new Scanner(new File("C:\\imports.txt"));
+
+                System.out.println(sf);
+            }
+        }
     }
-}
+
