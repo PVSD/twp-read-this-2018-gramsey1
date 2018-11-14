9f72ebccbd7da17e3bb69892f9eacbd057d7007f
gramsey1 <graceramsey312@yahoo.com>
2018-11-13 19:51:33 -0500

    h

---
+++ src/com/company/parts.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Created by gramsey1 on 11/13/18
+ */
+public class parts {
+
+        public parts(String a){
+            String cut[] = a.split("\\s");
+            absences = Integer.parseInt(cut[cut.length-1]);
+            grade = Double.parseDouble(cut[cut.length-2].substring(0, 2));
+            //cuz Van Winkle makes things difficult
+            name = "";
+            for(int i=0; i<=cut.length-3; i++){
+                name += cut[i] + " ";
+            }
+
+        }
+        public String name;
+        public double grade;
+        public int absences;
+
+    }
+}
